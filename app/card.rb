dcfe4c783af896b60ce45c40ae7a66140e8de688
Sergey Avanesov <sergey.awanesov@gmail.com>
2015-11-22 16:49:00 +0300

    Pass card test

--- app/card.rb
+++ app/card.rb
@@ -1,14 +1,13 @@
 require File.expand_path('../provider.rb', __FILE__)
-require File.expand_path('../validator.rb',__FILE__)
+require File.expand_path('../validator.rb', __FILE__)
 
 class Card
   def initialize(number)
-    @number = number
-    @type = Provider.identity(number)
-    @valid = Validator.validate(number)
+    @number = number.chomp
   end
 
   def info
-    "#{@type}, #{@valid}"
+    return 'Enter a credit card number' unless /^(?<num>\d+)$/ =~ @number
+    "#{Provider.identity(@number)}, #{Validator.validate(@number)}"
   end
 end
--- test/card_test.rb
+++ test/card_test.rb
@@ -4,12 +4,9 @@ require File.expand_path('../fixtures.rb', __FILE__)
 
 class CardTest < Test::Unit::TestCase
   def test_info
-    visa = Fixtures::Valid.get[:visa].first
-    card = Card.new(visa)
-    assert_equal(card.info, 'Visa, valid')
-
-    invalid_visa = Fixture::Invalid.get[:visa][:luhn].first
-    card = Card.new(invalid_visa)
-    assert_equal(card.info, 'Visa, invalid')
+    Fixtures::CardExample.get.each do |fixture|
+      card = Card.new(fixture[:number])
+      assert_equal(card.info, fixture[:info])
+    end
   end
 end
